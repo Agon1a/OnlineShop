154b50aa80ae717e73ad367e0795740b632af2a3
Agon1a <d.lozhkien@gmail.com>
2024-03-25 01:57:06 +1000

    finally можно скушать

---
+++ OnlineShop/Controllers/OrderControlller.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShop.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: /Order/Checkout
+        public IActionResult Checkout()
+        {
+            // Здесь можно добавить логику для оформления заказа
+            // Например, создание нового заказа в базе данных и перенаправление пользователя на страницу подтверждения заказа
+            return RedirectToAction("Confirmation"); // Перенаправляем пользователя на страницу подтверждения заказа
+        }
+
+        // GET: /Order/Confirmation
+        public IActionResult Confirmation()
+        {
+            // Здесь можно отобразить страницу с подтверждением заказа
+            return View();
+        }
+    }
+}
