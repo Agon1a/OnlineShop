116657b8476174ef65ff5e45e81019a3d28d6c0c
Agon1a <d.lozhkien@gmail.com>
2024-03-25 00:01:43 +1000

    TEST

---
+++ OnlineShop/Lib/IO/IShoppingCartService.cs
@@ -0,0 +1,8 @@
+﻿namespace OnlineShop.Lib.IO
+{
+    public interface IShoppingCartService
+    {
+        Task AddToCartAsync(string userId, Guid productId);
+        Task RemoveFromCartAsync(string userId, Guid productId);
+    }
+}
